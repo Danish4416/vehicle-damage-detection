f87cc74c8446808e9c00a14c9f468a52309f419c
Danish Ansari <danishansari7864416@gmail.com>
2026-08-15 21:03:48 +0530

    Initial commit: vehicle damage detection project

---
+++ streamlit_ui/app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import requests
+
+st.title("Vehicle Damage Detection")
+
+uploaded_file = st.file_uploader(
+    "Upload the file",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file:
+
+    st.image(
+        uploaded_file,
+        caption="Uploaded File",
+        width="stretch"
+    )
+
+    files = {
+        "file": (
+            uploaded_file.name,
+            uploaded_file.getvalue(),
+            uploaded_file.type
+        )
+    }
+
+    response = requests.post(
+        "http://127.0.0.1:8000/predict",
+        files=files
+    )
+
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Predicted Class: {result['prediction']}")
+    else:
+        st.error(f"API Error: {response.status_code} - {response.text}")
